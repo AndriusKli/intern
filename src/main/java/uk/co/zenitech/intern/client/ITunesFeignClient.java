b961783274168aca4c732584de3b4d9e50775421
andrius.kliunka <andrius.kliunka@zenitech.co.uk>
2020-10-08 09:05:57 +0100

    Fixed Feign.

--- src/main/java/uk/co/zenitech/intern/client/ITunesFeignClient.java
+++ src/main/java/uk/co/zenitech/intern/client/ITunesFeignClient.java
@@ -1,6 +1,7 @@
 package uk.co.zenitech.intern.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +15,16 @@ import javax.validation.constraints.Min;
 @Validated
 public interface ITunesFeignClient {
 
-    @GetMapping(value = "search?term={searchTerm}&entity={entity}&attribute={attribute}&limit=200")
+    @GetMapping(value = "search?term={searchTerm}&entity={entity}&attribute={attribute}&limit=200", consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<ITunesResponse> getResults(@RequestParam String searchTerm,
                                       @RequestParam String entity,
                                       @RequestParam String attribute,
                                       @RequestParam(required = false) @Min(1) @Max(200) Long limit
     );
 
-    @GetMapping(value = "lookup?id={id}")
+    @GetMapping(value = "lookup?id={id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<ITunesResponse> getById(@RequestParam Long id);
 
-    @GetMapping(value = "lookup?id={id}&entity=song")
+    @GetMapping(value = "lookup?id={id}&entity=song", consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<ITunesResponse> getAlbumSongs(@RequestParam Long id);
 }
